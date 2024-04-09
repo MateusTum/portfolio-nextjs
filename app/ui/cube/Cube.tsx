68b6b2cdcdb101e20471c9acc369f93a760a29e7
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-04-09 11:59:13 -0300

    ui - cube

---
+++ app/ui/cube/Cube.tsx
@@ -0,0 +1,18 @@
+import styles from "./cube.module.scss";
+
+export default function rotatingCube() {
+  return (
+    <div className={styles.cubeWrapper}>
+      <div className={styles.scene}>
+        <div className={styles.cube}>
+          <div className={`${styles.face} ${styles.front}`}></div>
+          <div className={`${styles.face} ${styles.back}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.bottom}`}></div>
+        </div>
+      </div>
+    </div>
+  );
+}
