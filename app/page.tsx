af7f1bd87d10ae585bff8248fd08981e3c5f3841
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-04-09 15:03:23 -0300

    Create projects route

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ import {
   siSqlite,
   siPostgresql,
   siMongodb,
+  siWhatsapp,
 } from "simple-icons/icons";
 
 export const metadata: Metadata = {
@@ -56,7 +57,7 @@ function PresentationSection() {
         <h2>Frontend | Backend</h2>
         <h2>Next.js | Django</h2>
       </div>
-      <div className="relative h-[400px] bottom-0 right-0">
+      <div className="absolute h-[400px] md:bottom-[50px] md:right-[50px] bottom-0 right-0">
         <RotatingCube />
       </div>
     </Section>
@@ -66,10 +67,10 @@ function PresentationSection() {
 function AboutSection() {
   return (
     <Section id="about">
-      <div className="text-4xl text-center bg-neutral-900 w-[50%] mx-auto rounded-md p-2">
+      <div className="w-full md:w-[75%] lg:w-[50%] text-4xl text-center bg-neutral-950/50  mx-auto rounded-md p-2">
         <h2>ABOUT ME</h2>
       </div>
-      <div className="px-2 my-1 w-[50%] mx-auto">
+      <div className="w-full md:w-[75%] lg:w-[50%] px-2 my-1 mx-auto">
         <p className="text-lg text-justify">
           My name is <span className="text-red-500">Mateus Rosa Tum</span>,{" "}
           <span className="text-red-500">24</span>, from
@@ -147,27 +148,25 @@ function MyWorkSection() {
   };
 
   return (
-    <section id="my-work" className="w-full my-20 px-4 justify-center flex">
-      <ul className="grid grid-cols-8 gap-4">
-        {Object.keys(mytech).map((key) => {
-          return (
-            <li
-              key={key}
-              className="col-span-1 h-[128px] w-[128px] rounded-md flex items-center justify-center bg-neutral-900/25 backdrop-blur-lg"
-            >
-              <a href={mytech[key].url} target="_blank" rel="noreferrer">
-                <svg
-                  viewBox="0 0 24 24"
-                  width="64"
-                  height="64"
-                  className="w-full h-full max-w-[64px] max-h-[64px]"
-                >
-                  <path d={mytech[key].icon.path} fill="currentColor" />
-                </svg>
-              </a>
-            </li>
-          );
-        })}
+    <section id="my-tech" className="w-full my-20 px-4 flex justify-center">
+      <ul className="flex overflow-x-auto snap-x snap-mandatory gap-4 sm:grid sm:grid-cols-8">
+        {Object.keys(mytech).map((key) => (
+          <li
+            key={key}
+            className="mx-auto snap-center min-w-[64px] md:min-w-[92px] lg:min-w-[128px] h-[64px] md:h-[92px] lg:h-[128px] rounded-md flex items-center justify-center bg-neutral-900/25 backdrop-blur-lg sm:col-span-1"
+          >
+            <a href={mytech[key].url} target="_blank" rel="noreferrer">
+              <svg
+                viewBox="0 0 24 24"
+                width="64"
+                height="64"
+                className="w-[50%] h-[50%] md:w-[75%] md:h-[75%] lg:w-full lg:h-full max-w-[64px] max-h-[64px] mx-auto"
+              >
+                <path d={mytech[key].icon.path} fill="currentColor" />
+              </svg>
+            </a>
+          </li>
+        ))}
       </ul>
     </section>
   );
@@ -202,13 +201,17 @@ function PortfolioSection() {
       <div className="text-4xl text-center mx-auto p-2 my-2">
         <h2>MY PORTFOLIO</h2>
       </div>
-      <ul className="grid grid-rows grid-cols-2 gap-2 p-2">
+      <div>
+        <h3></h3>
+      </div>
+
+      <ul className="grid grid-rows md:grid-cols-2 lg:grid-cols-3 gap-2 p-2">
         {/* Grid 1 */}
         {Object.keys(myProjects).map((key) => {
           return (
             <li
               key={key}
-              className="flex-row items-center w-full h-[350px] mx-auto rounded-lg overflow-hidden"
+              className="flex-row items-center w-full h-[250px] mx-auto rounded-lg overflow-hidden"
             >
               <div className="bg-black h-4/5 w-full"></div>
               <div className="bg-neutral-950 h-1/5 w-full relative">
@@ -233,9 +236,44 @@ function PortfolioSection() {
 
 function ContactSection() {
   return (
-    <Section id="contact">
-      <div className="text-4xl text-center bg-neutral-900 w-[50%] mx-auto rounded-md p-2 my-20">
+    <Section
+      id="contact"
+      classes="py-20 min-h-[50vh] bg-gradient-to-b from-black from-60% to-neutral-950/50"
+    >
+      <div className="w-full md:w-[50%] text-4xl text-center bg-neutral-950/50  mx-auto rounded-md p-2">
         <h2>CONTACT ME</h2>
+      </div>
+      <div className="p-2 grid grid-rows-3 gap-1 sm:gap-3">
+        <div>
+          <h3 className="text-3xl">Email</h3>
+          <a
+            className="text-neutral-400 text-xl"
+            href="mailto:mateus.tum@icloud.com"
+          >
+            mateus.tum@icloud.com
+          </a>
+        </div>
+        <div>
+          <h3 className="text-3xl">Phone Number</h3>
+          <a className="text-neutral-400 text-xl" href="tel:+5564999549530">
+            +55 (64) 9-9954-9530
+          </a>
+        </div>
+
+        <div>
+          <h3 className="text-3xl">WhatsApp</h3>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="text-neutral-400 flex"
+            href="https://wa.me/+55649995495430?text=Hello%2C%20I%27m%20interested%20in%20your%20web%20development%20services.%20Can%20we%20discuss%20further%3F"
+          >
+            <svg className="w-8 h-8" viewBox="0 0 32 32" width="32" height="32">
+              <path d={siWhatsapp.path} fill="currentColor" />
+            </svg>
+            Click here to send me a message on WhatsApp
+          </a>
+        </div>
       </div>
     </Section>
   );
---
+++ app/project/[id]/page.tsx
@@ -0,0 +1,11 @@
+export default function ProjectPage() {
+    return (
+      <main className="flex-row align-center min-h-screen">
+        <div className="pt-[60px]">
+            
+        <p>sadiihjasijdas</p>
+
+        </div>
+      </main>
+    );
+  }
