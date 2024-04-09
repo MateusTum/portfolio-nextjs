f0f4e1f48bc4325486a287e6912dabcf0471b66d
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-04-09 11:59:21 -0300

    Create navbar.tsx

---
+++ app/ui/navbar/navbar.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { useState } from 'react';
+import navLinks from "@/app/lib/navLinks";
+import styles from './navbar.module.scss';
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="fixed top-0 w-full bg-neutral-900/50 p-2 backdrop-blur-lg z-50">
+      <div className="flex items-center max-h-[40px] overflow-hidden">
+        <div className="absolute p-1 bg-black rounded-lg text-sm">
+          <span>Portfolio v2.0</span>
+        </div>
+        <button
+          className="mx-1 ms-auto text-white md:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? 'Close' : 'Menu'}
+        </button>
+        {/* Large navbar */}
+        <nav
+        className={`${
+          isOpen ? 'block' : 'hidden'
+        } hidden md:block w-full md:w-[50%] mx-auto`}
+      >
+        <ul className="flex flex-col md:flex-row w-full justify-around">
+          {Object.keys(navLinks).map((key) => {
+            return (
+              <li key={key}>
+                <div className="w-max relative">
+                  <a className={`uppercase text-lg ${styles["nav-link"]}`} href={`#${key}`}>
+                    {navLinks[key].title}
+                  </a>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+      </div>
+      {/* Small navbar */}
+      <nav
+        className={`${
+          isOpen ? 'block' : 'hidden'
+        } w-full md:w-[50%] mx-auto`}
+      >
+        <ul className="flex flex-col md:flex-row w-full justify-around">
+          {Object.keys(navLinks).map((key) => {
+            return (
+              <li key={key}>
+                <div className="w-max relative">
+                  <a className={`uppercase text-lg ${styles["nav-link"]}`} href={`#${key}`}>
+                    {navLinks[key].title}
+                  </a>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </div>
+  );
+}
