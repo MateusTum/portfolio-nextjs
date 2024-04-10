ca79aff613461ec02c51f2bce784eee5266677af
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-04-10 11:30:22 -0300

    Update definitions.ts
    add mytech interface

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,6 +1,17 @@
+import type { SimpleIcon } from "simple-icons";
+
 export interface sectionProps {
-    children: React.ReactNode;
-    fullscreen?: boolean;
-    id: string;
-    classes?: string;
-  }
+  children: React.ReactNode;
+  fullscreen?: boolean;
+  id: string;
+  classes?: string;
+}
+
+export interface mytech {
+  [key: string]: {
+    name: string;
+    description: string;
+    url: string;
+    icon: SimpleIcon;
+  };
+}
