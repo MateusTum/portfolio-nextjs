acb46e2f9c0d6f7bf41378584198a26c59482dd2
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-04-11 11:17:31 -0300

    Update tailwind.config.ts
    Custom animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+        animation: {
+          'pulse-slow': 'pulse 3s linear infinite',  // Example of a slower pulse
+          'pulse-fast': 'pulse 1s linear infinite'   // Example of a faster pulse
+        }
+
     },
     container: {
       padding: {
