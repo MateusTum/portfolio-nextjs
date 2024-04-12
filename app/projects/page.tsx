b9a6667c3ea5c3e352fbcdf73aa745e2473b47eb
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-04-12 14:04:45 -0300

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,21 @@
-export default function ProjectPage() {
-    return (
-      <div className="flex flex-row">
-        <p>page under construction</p>
+import type { Metadata } from "next";
+import ProjectsViewer from "@/app/ui/projectsViewer/viewer";
 
+export const metadata: Metadata = {
+  title: "Mateus Tum | Portfolio",
+  description: "Mateus Tum Projects",
+};
+
+export default function ProjectPage() {
+  return (
+    <div className="container">
+
+      <div className="flex w-full h-max">
+        <h1 className="text-4xl text-center w-full my-1"> Portfolio | Under Construction </h1>
       </div>
-    );
-  }
+
+      <ProjectsViewer />
+
+    </div>
+  );
+}
