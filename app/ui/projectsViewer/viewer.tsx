e41c28c7398d5715b36f151979ae9f94cfe1e8e0
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-04-12 14:04:54 -0300

    Create viewer.tsx

---
+++ app/ui/projectsViewer/viewer.tsx
@@ -0,0 +1,82 @@
+import { TechList } from "@/app/lib/data";
+import { MyProjects } from "@/app/lib/data";
+import Project from "./project";
+
+export default function projectsViewer() {
+  const sortFilters = ["Latest", "Oldest", "Version"];
+  const sourceArray = ["Any", "Open", "Closed"];
+
+  return (
+
+      <div className="flex w-full rounded-md overflow-hidden my-4">
+
+        <div className="hidden sm:block w-1/6 flex-row text-lg bg-neutral-950">
+
+          <div className="p-2">
+            <div className="text-center mx-auto bg-red-700 px-2 rounded-md w-max h-min my-1">
+              <span>Sort by</span>
+            </div>
+
+            <div>
+              <ul className="border border-neutral-500 rounded-md grid grid-rows">
+                {sortFilters.map((filter) => (
+                  <li className="p-1" key={filter}>{filter}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          <div className="p-2 text-left">
+            <div className="text-center mx-auto bg-red-700 px-2 rounded-md w-max h-min my-1">
+              <span>Techs</span>
+            </div>
+
+            <div>
+              <ul className="border border-neutral-500 grid grid-cols-3 rounded-md p-1">
+                {Object.entries(TechList).map(([key, value]) => (
+                  <li key={key} className="mx-auto m-1 ring-2 ring-black rounded-md overflow-hidden">
+                    <svg viewBox="-4 -4 32 32" width="32" height="32">
+                      <path d={value.icon.path} fill="currentColor" />
+                    </svg>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          <div className="p-2 text-center">
+            <div className="text-center mx-auto bg-red-700 px-2 rounded-md w-max h-min my-1">
+              <span>Source</span>
+            </div>
+
+
+            <div>
+              <ul className="border border-neutral-500 rounded-md grid grid-rows text-left">
+                {sourceArray.map((source) => (
+                  <li key={source} className="p-1">{source}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          <div className="text-center p-2">
+            <button className="bg-green-800 px-2 py-1 rounded-md">
+              Apply filters
+            </button>
+          </div>
+        </div>
+
+        {/* Projects div */}
+        <div className="flex flex-col w-full sm:w-5/6">
+          <ul className="grid grid-cols grid-rows bg-neutral-900 gap-4 p-2
+          sm:grid-cols-4 sm:grid-rows-3">
+          {Object.entries(MyProjects).map(([ProjectName, ProjectInfo]) => (
+            <Project key={ProjectName} project={ProjectInfo} />
+          ))}
+          </ul>
+        </div>
+      
+      </div>
+  );
+}
