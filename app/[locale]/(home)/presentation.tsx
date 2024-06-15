7c2df6ec2ccdbe6d3b855ca051c207a96bb8aa57
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-06-15 11:55:29 -0300

    Update presentation.tsx

--- app/[locale]/(home)/presentation.tsx
+++ app/[locale]/(home)/presentation.tsx
@@ -8,8 +8,7 @@ export default function PresentationSection({ t }: any) {
         <div className="text-4xl uppercase mt-9">
           <h1>Mateus Tum</h1>
           <h2>{t('occupation')}</h2>
-          <h2>Frontend | Backend</h2>
-          <h2>Next.js | Django</h2>
+          <h2>Full-stack</h2>
         </div>
         <div className="absolute w-full sm:w-[400px] h-[300px] bottom-0 right-0">
           <RotatingCube />
